25bab09f599bd5d3500cfcb7e8397ae9978e07b5
mirabelenkiy-circle <mira.belenkiy@contractor.circle.com>
2023-10-12 09:31:32 -0400

    Accountable CGG21 and GG18 (#6)
    - zk proofs
    - Accountable GG18 with zk proofs
    - Accountable CGG21 with zk proofs
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Trevor Baker <tjbaker@users.noreply.github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

---
+++ ecdsa/accsigning/debuglog.go
@@ -0,0 +1,23 @@
+// Copyright 2023 Circle
+
+package accsigning
+
+import (
+	"fmt"
+	"os"
+)
+
+const logfile = "accountablelog.txt"
+
+func Logf(format string, a ...any) (n int, err error) {
+	msg := fmt.Sprintf(format, a...)
+	msg = fmt.Sprintf("%s\n", msg)
+	f, err := os.OpenFile(logfile, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		n = 0
+		return
+	}
+	defer f.Close()
+	n, err = f.WriteString(msg)
+	return
+}
